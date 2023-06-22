caa1d8af7001f144862953d04b8c012594e54b34
temirlan <k.temirlan.t@gmail.com>
2023-06-22 20:22:49 +0600

    Initial Commit

---
+++ HSTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HSTestTask
+//
+//  Created by Темирлан Кудайберген on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
